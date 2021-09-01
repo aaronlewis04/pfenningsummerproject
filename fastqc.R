69604ca4892647acbfec10b925a43e7a8c02047d
lewwwis <aaronvivlewis@gmail.com>
2021-09-01 00:57:17 -0400

    Add files via upload

---
+++ fastqc.R
@@ -0,0 +1,12 @@
+library("fastqcr")
+
+#connected via to the cmu cluster with ssfhs which i downloaded from a github repo following instructions from user @thallgren on this page : https://github.com/telepresenceio/telepresence/issues/1654
+
+fastqc(fq.dir = "/Users/aaronlewis/sshfs/projects/pfenninggroup/rada/single_cell", # FASTQ files directory
+       qc.dir = "/Users/aaronlewis/sshfs/projects/pfenninggroup/rada/single_cell", # Results directory
+       threads = 4                    # Number of threads
+)
+
+#in the same folder after running this one function was an html file for each fastq sample with the qc figures for each.
+
+
